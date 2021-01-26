bce8127ea5943cbfbd6fda778574e9a053bcd73e
og-analytics <og-analytics@oilgainsanalytics.com>
2021-01-25 22:10:14 -0600

    remove folder and file if exist. needed during provisioning

--- scripts/provision-vagrant-user.sh
+++ scripts/provision-vagrant-user.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 echo "Install pyenv as user"
+echo "Ensure pyenv folder and .bash_profile file are gone"
+if [[ ! -f /home/vagrant/.pyenv ]]; then
+	rm -rf /home/vagrant/.pyenv
+fi
+if [[ ! -f ~/.bash_profile ]]; then
+	rm ~/.bash_profile
+fi
 echo "create ~/.bash_profile"
 touch ~/.bash_profile
 grep -q -F "export PATH=\"/home/vagrant/.pyenv/bin:\$PATH\"" ~/.bash_profile || echo "export PATH=\"/home/vagrant/.pyenv/bin:\$PATH\"" >> ~/.bash_profile
+echo "attempt to load .bash_profile"
 source ~/.bash_profile
+echo "add pyenv to .bash_profile"
 grep -q -F "eval \"\$(pyenv init -)\"" ~/.bash_profile || echo "eval \"\$(pyenv init -)\"" >> ~/.bash_profile
 grep -q -F "eval \"\$(pyenv virtualenv-init -)\"" ~/.bash_profile || echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bash_profile
 curl -s -S -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
@@ -22,7 +31,7 @@ echo "Activate pyenv on ./data folder with anaconda3-2020.07"
 pyenv local anaconda3-2020.07       #? set folder with specified environment Python 3.6.4
 # conda list
 
-echo "Install deep learning packages"
+echo "Install Deep Learning packages"
 conda install -y theano tensorflow keras 
 
 echo "Install plotnine"
@@ -33,6 +42,7 @@ pip install welly
 
 echo "Setup login directory"
 echo "cd /home/vagrant/data/"  >> /home/vagrant/.bash_profile
+# load .bashrc after .bash_profile
 echo "source ~/.bashrc"  >> /home/vagrant/.bash_profile
 
 # conda update -n base conda
